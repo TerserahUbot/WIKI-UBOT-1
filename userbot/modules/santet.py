b20e4f42e886177648c1e1153cef15a36a622c05
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2021-12-10 22:02:40 +0800

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,11 +6,11 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(f"**{ALIVE_NAME} Pengen Nyantet Si Kontol Ini!**")
+    await typew.edit(f"**{ALIVE_NAME} Pengen Nyantet Si Bocil!**")
     sleep(2)
-    await typew.edit("__Mencari Nama Si Kontol...__")
+    await typew.edit("__Mencari Nama Si Bocil...__")
     sleep(2)
-    await typew.edit("__Santet Online Segera Dilakukan,Mampus Lu Jelek!!__")
+    await typew.edit("__Santet Online Segera Dilakukan,Mampus Lu Cil!!__")
     sleep(2)
     await typew.edit("0%")
     number = 1
@@ -315,7 +315,7 @@ async def typewriter(typew):
     sleep(2)
     await typew.edit("__Paku, Pisau, Gunting Jembut, Tanah Kuburan Bapak Lu, Tagihan Kost Udh Gua Masukin Di Badan Lu__")
     sleep(5)
-    await typew.edit("__Si Jelek Udah Disantet Onlen:)__")
+    await typew.edit("__Si Bocil Udah Disantet Onlen:)__")
 
 
 CMD_HELP.update({
